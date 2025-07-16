f3f6433b2daaddde380d14eb3b1aa9f1947ad88a
feden-pe <filipeedensilva@gmail.com>
2025-07-16 13:13:36 +0100

    Getting more stuff ready

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -13,14 +13,17 @@ if [ -d "/var/lib/mysql/$DB_NAME" ]; then
 else
 	echo "[mariadb:setup.sh] Database '$DB_NAME' doesn't exist, creating it now!"
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql
-	mysqld_safe --user=mysql --datadir=/var/lib/mysql &
-	sleep 3
+	mysqld --user=mysql --datadir=/var/lib/mysql &
+	until mysqladmin ping --silent; do
+		sleep 1
+	done
 	mysql -u root <<EOF
 	CREATE DATABASE IF NOT EXISTS $DB_NAME;
 	CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
 	GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 EOF
 	mysqladmin -uroot -p$DB_ROOT_PASSWORD shutdown
 	sleep 2
 fi
-exec mysqld_safe --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
+exec mysqld_safe --user=mysql --datadir=/var/lib/mysql
